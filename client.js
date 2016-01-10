eac0c96a2b6f0657ee2664c8266d7008b0e816d1
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-09 16:21:07 -1000

    server allows two clients to talk to eachother, but i have to remove them from the array when the clients leave

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
 var net = require('net');
 
-var client = net.connect({port:8080,host:'localhost'}, function(){
+var client = net.connect({port:6969,host:'localhost'}, function(){
 
   client.on('data',function(data){
     console.log(data.toString());
   });
 
   process.stdin.setEncoding('utf8');
+
+  process.stdin.on('data', function(data){
+    client.write(data);
+  });
 
 
 
--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 var net = require('net');
 var server = net.createServer(connect);
+var clientManager = [];
+
 
 //this will go off when a client connects to the server
 function connect(client){
+  clientManager.push(client);
 
+  //to tell the server who is connected
+  console.log("CONNECTED: "+ address.address +":"+ client.remotePort);
 
+  //goes to anyone who connects
+  client.write('Welcome to the Server!!!');
 
+  //when the person who connects sends data to the server
+  client.on('data', function(data){
+    console.log('SERVER BCAST FROM '+ address.address +":"+client.remotePort+": "+data.toString());
+    client.write(address.address+':'+client.remotePort+": "+ data.toString());
+  });
+
+  //**** need to figure how to figure our which client is talking  if
+  if(clientManager.length===2){
+    clientManager[0].on('data',function(data){
+      clientManager[1].write(address.address+":"+client.remotePort+": "+data.toString());
+    });
+
+    clientManager[1].on('data', function(data){
+      clientManager[0].write(address.address+":"+client.remotePort+": "+data.toString());
+    });
+  }
+
+  client.on('end', function(){
+    console.log('CLOSED: '+address.address+":"+client.remotePort);
+  });
 
 }
 
 
-
-
-
 // this will go off when the server is turn on
-server.listen({port:8080,host:'localhost'},function(){
+server.listen({port:6969,host:'localhost',address:'0.0.0.0'},function(){
   address = server.address();
   console.log('opened server on %j', address);
 });
+
+
