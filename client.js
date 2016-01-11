fbb00fd459f326c7fd50be1b9777d99c4703c071
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-11 13:39:25 -1000

    server can talk to the clients

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 var net = require('net');
 
 var client = net.connect({port:6969,host:'localhost'}, function(){
+  process.stdin.setEncoding('utf8');
 
   client.on('data',function(data){
     console.log(data.toString());
   });
 
-  process.stdin.setEncoding('utf8');
 
   process.stdin.on('data', function(data){
     client.write(data);
--- server.js
+++ server.js
@@ -1,70 +1,71 @@
 var net = require('net');
+var fs = require('fs');
 var server = net.createServer(connect);
 var clientManager = [];
 
 
 //this will go off when a client connects to the server
 function connect(client){
-  clientManager.push(client);
+  //client.username = alert('What is your username?');
 
-  //to tell the server who is connected
-  console.log("CONNECTED: "+ address.address +":"+ client.remotePort);
+  client.setEncoding('utf-8');
 
-  console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
-  console.log(clientManager);
-  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-
-  //goes to anyone who connects
-  client.write('Welcome to the Server!!!');
-
-  //when the person who connects sends data to the server
-  client.on('data', function(data){
-    console.log('SERVER BCAST FROM '+ address.address +":"+client.remotePort+": "+data.toString());
-    client.write(address.address+':'+client.remotePort+": "+ data.toString());
+  client.write('Please type in the word username\nfollowed by your your choice of username like so\nusername: your username');
+  client.on('data',function(data){
+    if(data.search(/username:/i) >= 0){
+      var splitData = data.split(' ');
+      client.username = splitData[1];
+      console.log(client.username);
+      client.write('Welcome to the Server '+client.username+'!');
+    }
   });
 
+  clientManager.push(client);
+
+  console.log("CONNECTED: "+ address.address +":"+ client.remotePort);
+
+  client.on('data', function(data){
+    if(client.username !== undefined){
+      console.log('SERVER BCAST FROM '+ client.username+": "+data.toString());
+      client.write(client.username+"==> "+ data.toString());
+    } else{
+      console.log('SERVER BCAST FROM '+ address.address +":"+client.remotePort+": "+data.toString());
+      client.write(address.address+':'+client.remotePort+"==> "+ data.toString());
+    }
+  });
 
   client.on('data',function(data){
     for(var j=0 ; j< clientManager.length ; j++){
       if(client!==clientManager[j]){
-        clientManager[j].write(address.address+":"+client.remotePort+": "+data.toString());
+        if(client.username !== undefined){
+          console.log('SERVER BCAST FROM '+ client.username+": "+data.toString());
+          clientManager[j].write(client.username+"==> "+ data.toString());
+        } else{
+          console.log('SERVER BCAST FROM '+ address.address +":"+client.remotePort+": "+data.toString());
+          clientManager[j].write(address.address+':'+client.remotePort+"==> "+ data.toString());
+        }
       }
     }
   });
-
-  // client.on('data',function(data){
-  //   //for(var j=0 ; j< clientManager ; j++){
-  //     clientManager[0].write(address.address+":"+client.remotePort+": "+data.toString());
-  //   //}
-  // });
-
-  //   client.on('data',function(data){
-  //   //for(var j=0 ; j< clientManager ; j++){
-  //     clientManager[1].write(address.address+":"+client.remotePort+": "+data.toString());
-  //   //}
-  // });
-
 
   client.on('end', function(){
     console.log('CLOSED: '+address.address+":"+client.remotePort);
     for(var i = 0;i<clientManager.length;i++){
       console.log(client.remotePort);
-
-
       if(clientManager[i]===client){
-
         clientManager.splice(i,1);
         console.log('this is the amount '+ clientManager.length);
-
       }
-
     }
+  });
 
-
+  process.stdin.on('data',function(data){
+    for(var j=0 ; j< clientManager.length ; j++){
+    clientManager[j].write("ADMIN ==> "+ data.toString());
+    }
   });
 
 }
-
 
 // this will go off when the server is turn on
 server.listen({port:6969,host:'localhost',address:'0.0.0.0'},function(){
